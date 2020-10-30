0ac28623c19dd0e5c282e6ceb552841e419e8e7f
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-10-30 15:24:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <title>Lab-11</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Apirat Jamnongrat</h1>
+        <p>นายอภิรัฐ จำนงค์รัตน์</p>
+        <p>ชื่อเล่น ปาล์ม</p>
+        <img src="cat.jpg">
+        <img src="cat2.jpg">
+        <br>
+        <a href="https://www.netflix.com/th-en/">This is a Link</a>
+
+        <h2>My favorite song</h2>
+        <ul>
+            <li>Sakura</li>
+            <li>Overfly</li>
+            <li>Mabataki</li>
+        </ul>
+        <br>
+        <h2>เรื่องตลกที่จะเล่า</h2>
+        <p>คู่แต่งงานใหม่เพิ่งจะถูกส่งตัวเข้าห้องหอ<br>
+            " น้องมีเรื่องจะสารภาพค่ะ" เจ้าสาวหมาดๆ บอก<br>
+            " น้อง เคยผ่านผู้ชายมาแล้ว"<br>
+            " ไม่เป็นไรหรอกพี่เข้าใจ" เจ้าบ่าวหมาดๆ ปลอบ<br>
+            " พี่ก็เคยผ่านผู้ชายมาแล้วเหมือนกัน......ฮาไม่ฮาก็ขอลาไปก่อนนะครับ"</p>
+
+    </body>
+</html>
